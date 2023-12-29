148bd175de63404f344f4e86bac2f2dec8585e66
Nergal <damned.nergal@gmail.com>
2023-12-29 13:00:13 +0200

    Update app.service.ts

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,12 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return (
+      <script
+        src='//eu.fw-cdn.com/11031462/400576.js'
+        chat='true'
+        widgetId='9cb85206-db4f-498c-83a9-19ca82711f44'>
+      </script>
+    );
   }
 }
