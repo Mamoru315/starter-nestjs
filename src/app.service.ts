b9bb3bf36274fa6fcbe82fdfe9a296dbe0ce6559
Nergal <damned.nergal@gmail.com>
2024-02-05 08:37:16 +0200

    Update app.service.ts

--- src/app.service.ts
+++ src/app.service.ts
@@ -7,7 +7,7 @@ export class AppService {
       <script 
         src='//eu.fw-cdn.com/11031462/400576.js'
         chat='true'
-        widgetId='9cb85206-db4f-498c-83a9-19ca82711f44'>
+        widgetId='06890482-de27-44b4-a319-f34875964f14'>
       </script>
     `;
   }
